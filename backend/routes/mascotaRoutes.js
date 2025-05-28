db659d98588a681d17cbadd3248971e592d8e9bc
rojo garza <a10722763@tec.mx>
2025-05-27 19:03:04 -0600

    Eliminar backend del seguimiento en gh-pages

--- backend/routes/mascotaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createMascota } = require('../controllers/mascotaController');
-const verifyToken = require('../middleware/authMiddleware');
-
-router.post('/', verifyToken, createMascota);
-
-module.exports = router;
